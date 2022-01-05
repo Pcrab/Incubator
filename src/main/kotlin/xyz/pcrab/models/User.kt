b2408247b1205638cf374fab47ac275b959b42ab
Pcrab <pcrab@pcrab.xyz>
2022-01-05 13:30:52 +0800

    add SHA-256 encrypt support for password

--- src/main/kotlin/xyz/pcrab/models/User.kt
+++ src/main/kotlin/xyz/pcrab/models/User.kt
@@ -1,22 +1,27 @@
 package xyz.pcrab.models
 
 import kotlinx.serialization.Serializable
+import java.security.MessageDigest
 
 @Serializable
-data class User (
-    val username: String,
-    val password: String,
-    val serialNumber: String
+data class User(
+    val username: String, val password: String, val serialNumber: String
 )
 
 @Serializable
-data class UserSession (
+data class UserSession(
     val username: String
 )
+
+fun String.encryptThroughSHA256(): String {
+    return MessageDigest.getInstance("SHA-256").digest(this.toByteArray())
+        .joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
+}
+
 fun getUser(username: String, password: String): User? {
-    return getDbUser(username, password)
+    return getDbUser(username, password.encryptThroughSHA256())
 }
 
 fun createUser(username: String, password: String, serialNumber: String): String? {
-    return createDbUser(User(username, password, serialNumber))
+    return createDbUser(User(username, password.encryptThroughSHA256(), serialNumber))
 }
