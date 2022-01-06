fc88672c4962fa89dfc2c7e9216edb5e04e1df8e
Pcrab <pcrab@pcrab.xyz>
2022-01-07 01:06:32 +0800

    change sha-256 to sha3-512

--- src/main/kotlin/xyz/pcrab/models/User.kt
+++ src/main/kotlin/xyz/pcrab/models/User.kt
@@ -8,15 +8,20 @@ data class User(
     val username: String, val password: String, val serialNumber: String?
 )
 
-fun String.encryptThroughSHA256(): String {
-    return MessageDigest.getInstance("SHA-256").digest(this.toByteArray())
-        .joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
+fun String.encryptThroughSHA3512(): String {
+    var str = this
+    for (i in 1 ..100) {
+        str += i.toString()
+        str = MessageDigest.getInstance("SHA3-512").digest(this.toByteArray())
+            .joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
+    }
+    return str
 }
 
 fun getUser(username: String, password: String): User? {
-    return getDbUser(username, password.encryptThroughSHA256())
+    return getDbUser(username, password.encryptThroughSHA3512())
 }
 
 fun createUser(username: String, password: String, serialNumber: String): String? {
-    return createDbUser(User(username, password.encryptThroughSHA256(), serialNumber))
+    return createDbUser(User(username, password.encryptThroughSHA3512(), serialNumber))
 }
