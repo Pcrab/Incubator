007649bc4ffc10b7c5b070ff30d28f1cf6bd4a71
Pcrab <pcrab@pcrab.xyz>
2022-02-24 15:08:37 +0800

    change format

--- src/main/kotlin/xyz/pcrab/models/Incubator.kt
+++ src/main/kotlin/xyz/pcrab/models/Incubator.kt
@@ -12,9 +12,10 @@ data class IncubatorControl(
     val temperatureHigh: Double, // 5 0xx.x
     val light: Int, // 1
     val dust: Int, // 1
+    val days: Int, // 1
 )
 
-const val incubatorControlLength = 13
+const val incubatorControlLength = 14
 
 @Serializable
 data class IncubatorControlGroup(
@@ -40,8 +41,8 @@ data class Incubator(
     val mode: Int, // 1
     val temperature: Double, // 5 xx.x
     val co2: Int, // 5
-    val light: Int, // 5
-    val dust: Int, // 5
+    val light: Boolean, // 1
+    val dust: Boolean, // 1
     val water: Int, // 2
     val pi: Boolean,
     val fan1: Boolean,
@@ -49,11 +50,10 @@ data class Incubator(
     val pump: Boolean,
     val beep: Boolean,
     val led: Boolean,
-    val time: Int, // 8
-    val days: Int, // 1
+    val time: String, // 8
 )
 
-private const val incubatorLength = 38
+private const val incubatorLength = 30
 
 @Serializable
 data class IncubatorGroup(
@@ -78,7 +78,8 @@ private fun buildIncubatorControl(content: String): IncubatorControl {
     val temperatureLow = content.substring(6..10).toDouble()
     val light = content[11].toString().toInt()
     val dust = content[12].toString().toInt()
-    return IncubatorControl(id, temperatureLow, temperatureHigh, light, dust)
+    val days = content[13].toString().toInt()
+    return IncubatorControl(id, temperatureLow, temperatureHigh, light, dust, days)
 }
 
 private fun buildIncubator(content: String): Incubator {
@@ -86,17 +87,19 @@ private fun buildIncubator(content: String): Incubator {
     val mode = content[1].toString().toInt()
     val temperature = content.substring(2..6).toDouble()
     val co2 = content.substring(7..11).toInt()
-    val light = content.substring(12..16).toInt()
-    val dust = content.substring(17..21).toInt()
-    val water = content.substring(22..23).toInt()
-    val pi = content[24] != '0'
-    val fan1 = content[25] != '0'
-    val fan2 = content[26] != '0'
-    val pump = content[27] != '0'
-    val beep = content[28] != '0'
-    val led = content[29] != '0'
-    val time = content.substring(30..37).toInt()
-    val days = if (content[38] == '0') 7 else 10
+    val light = content[12] != '0'
+    val dust = content[13] != '0'
+    val water = content.substring(14..15).toInt()
+    val pi = content[16] != '0'
+    val fan1 = content[17] != '0'
+    val fan2 = content[18] != '0'
+    val pump = content[19] != '0'
+    val beep = content[20] != '0'
+    val led = content[21] != '0'
+    val time = content.substring(22..23) + "天 " +
+            content.substring(24 ..25) + "时 " +
+            content.substring(26..27) + "分 " +
+            content.substring(28..29) + "秒"
     return Incubator(
         id,
         mode,
@@ -107,7 +110,6 @@ private fun buildIncubator(content: String): Incubator {
         water,
         pi, fan1, fan2, pump, beep, led,
         time,
-        days
     )
 }
 
@@ -123,12 +125,13 @@ fun getIncubatorControlGroupString(serialNumber: String): String {
     var str = ""
     for (incubatorControl in group.incubatorControls) {
         str += String.format(
-            "%01d%05.1f%05.1f%1d%1d",
+            "%01d%05.1f%05.1f%1d%1d%1d",
             incubatorControl.id,
             incubatorControl.temperatureHigh,
             incubatorControl.temperatureLow,
             incubatorControl.light,
             incubatorControl.dust,
+            incubatorControl.days
         )
     }
     println(str)
